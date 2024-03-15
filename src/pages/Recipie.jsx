bc92de7360c68013ef89f6c9d74516a6b248349a
Edinson <edinson.ningles@gmail.com>
2024-03-15 17:34:21 +0100

    Added news components

---
+++ src/pages/Recipie.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+function Recipie() {
+  return (
+    <div>Recipie</div>
+  )
+}
+
+export default Recipie
